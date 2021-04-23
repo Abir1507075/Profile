39744b35c268644d03fa0b4c2b13e7c59311de4f
Abid Hasan Abir <abidhasanabir19@gmail.com>
2021-04-24 03:54:59 +0600

    changed server.js file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/profile'));
+app.use(express.static('./dist/profile'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/profile/index.html'));
-});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/profile/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
